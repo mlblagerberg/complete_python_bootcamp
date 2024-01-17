63adc3769355e8adaecc907ed1b719d1839c0ab4
mlblagerberg <mlblagerberg@gmail.com>
2024-01-17 13:54:20 -0800

    Creating file for first project: simple band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,13 @@
+"""
+Project: Band Name Generator
+Start: January 17th, 2024
+Last touched: January 17th, 2024 
+Author: Madeleine L.
+"""
+
+### Program output
+# Welcome to the Band Name Generator.
+# What's the name of the city you grew up in?
+# What's your pet's name?
+
+print('Hello World')
