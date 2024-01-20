01eba355befee93f66f792ea891e5d92ad46c732
mlblagerberg <mlblagerberg@gmail.com>
2024-01-19 16:45:58 -0800

    Working with indexes.

--- Day4_randomization_lists.py
+++ Day4_randomization_lists.py
@@ -63,3 +63,53 @@ fruits[-1] = "Melons"
 print(fruits)
 
 print(dirty_dozen2[1][1])
+
+### Indexing
+# My initial solution
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+
+letter = position[0].upper()
+number = int(position[1])
+
+if letter == "A":
+  letter_position = 0
+elif letter == "B":
+  letter_position = 1
+elif letter == "C":
+  letter_position = 2
+
+# print(f"{letter} {letter_position} {number}")
+map[number - 1][letter_position] = "X"
+
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
+
+# Solution code using index()
+# My initial solution
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+
+number_index = int(position[1]) - 1
+
+map[number_index][letter_index] = "X"
+
+print(f"{line1}\n{line2}\n{line3}")
