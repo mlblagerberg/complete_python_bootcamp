8ca58813484c4235a018a9df516396fb83edaaa0
mlblagerberg <mlblagerberg@gmail.com>
2024-01-23 13:56:01 -0800

    Reeborg's world hurdles.

--- Day6_escape_maze.py
+++ Day6_escape_maze.py
@@ -6,7 +6,45 @@ Author: Madeleine L.
 """
 
 ### Functions & While loops
-def my_function():
-  print("Hello")
+## Using Reeborg's World
 
-my_function()
+## Hurdle 1 needs turn right and jump
+# Function for turning right
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# Define jump hurdle function
+def jump():
+    #move()
+    turn_left()
+
+    move()
+    turn_right()
+   
+    move()
+    turn_right()
+
+    move()
+    turn_left()
+
+## Hurdle 2 needs to jump six times
+# Define function to jump 6 hurdles exactly
+def jump_stop():
+    for hurdles in range(0,6):
+        if at_goal() is True:
+            break
+        else:
+            jump()
+
+## Hurdle 3 nees to recognize when we've reached the goal and stop
+def course():
+    while at_goal() is False:
+        if front_is_clear() is True:
+            move()
+        else:
+            jump()
+        
+## Hurdle 4 needs to check for wall height
+            
