a26a5e587a8239cc846795a361b8e64072ba0318
mlblagerberg <mlblagerberg@gmail.com>
2024-01-27 18:49:50 -0800

    Combining two functions into a single function.

--- Day8_cipher_program.py
+++ Day8_cipher_program.py
@@ -1,68 +1,52 @@
 """
 Project: Cipher Program
 Start: January 26th, 2024
-Last touched: January 26th, 2024 
+Last touched: January 27th, 2024 
 Author: Madeleine L.
 """
 
 import string
 import math
+import operator
 
 # Greet function
-def greet():
-    print("This is a greet function.")
-    print("It is meant to welcome a user to the program.")
-    print("Welcome user!")
+# def greet():
+#     print("This is a greet function.")
+#     print("It is meant to welcome a user to the program.")
+#     print("Welcome user!")
 
-greet()
+# greet()
 
 ### Cipher attempt before lesson
 # Get alphabet
 alpha_list = list(string.ascii_lowercase)
 # print(alpha_list)
 
-# Create function to take any word and cipher length and return an encoded word
-def cipher_encode(word, num):
-    # Create list to store encode index and characters of word
-    encode_index = []
-    encode_chr = []
-
-    word_list = list(word)
-    # print(word_list)
-    for i in range(0,len(word_list)):
-        letter = word_list[i]
- 
-        # decode_index.append(alpha_list.index(letter))
-        # setting encoded index
-        ind = (alpha_list.index(letter) + num) % 26
-        encode_index.append(ind)
-        
-        encode_chr.append(alpha_list[encode_index[i]])
-        encode_word = ''.join(encode_chr)
-    
-    print(f"\nYour encoded word is {encode_word}\n")
-
-# cipher_encode("trust",2)
+## Create single function that can either encode or decode
 
 # Create function to take encoded word and cipher length and return decoded word
-def cipher_decode(word, num):
+def caeser(word, num, direction):
     # Create list to store encode index and characters of word
-    decode_index = []
-    decode_chr = []
-
+    caeser_index = []
+    caeser_chr = []
     word_list = list(word)
-    # print(word_list)
+
+    if direction.lower() == "encode":
+        ops = operator.add
+    elif direction.lower() == "decode":
+        ops = operator.sub
+    else:
+        print("Please choose either encode or decode for your direction")
     for i in range(0,len(word_list)):
         letter = word_list[i]
- 
-        # decode_index.append(alpha_list.index(letter))
-        ind = (alpha_list.index(letter) - num) % 26
-        decode_index.append(ind)
+        # setting decode index
+        ind = ops(alpha_list.index(letter), num) % 26
+        caeser_index.append(ind)
         
-        decode_chr.append(alpha_list[decode_index[i]])
-        decode_word = ''.join(decode_chr)
+        caeser_chr.append(alpha_list[caeser_index[i]])
+        caeser_word = ''.join(caeser_chr)
     
-    print(f"\nYour decoded word is: {decode_word}\n") 
+    print(f"\nYour {direction}d word is: {caeser_word}\n") 
 
 ## User calls
 # Ask user whether they are encoding or decoding
@@ -78,24 +62,16 @@ else:
     in_out = input(f"\n{in_out} is not a valid input. Please choose encode or decode. ")
 
 # Ask user for word to encode/decode
-word = input(f"\nPlease provide the word you would like {in_out}d. ")
+word_in = input(f"\nPlease provide the word you would like {in_out}d. ")
 
-# Ask user for ceasar cipher length
+# Ask user for cipher length
 cipher_length = input("\nHow long is your cipher? ")
-# print(type(int(cipher_length)))
-if type(int(cipher_length)) is False:
-    cipher_length = int(input("\nPlease input an integer value for your cipher length."))
-# if isinstance(cipher_length, int):
-    # cipher_length = input(f"{cipher_length} must be an interger, please provide an input of type int. ")
-else:
-    cipher_length = int(cipher_length)
-    if in_out == "encode":
-        cipher_encode(word, cipher_length)
-    else:
-        cipher_decode(word, cipher_length)
+cipher_length = int(cipher_length)
+
+caeser(word=word_in,num=cipher_length,direction=in_out)
 
 ### Lesson notes
-print(math.ceil(7.3))
+# print(math.ceil(7.3))
 
 ## Prime number checker | Lesson solution much cleaner than mine
 def prime_checker(num):
@@ -108,4 +84,4 @@ def prime_checker(num):
     else:
         print("It's not a prime number.")
 
-prime_checker(10)
+# prime_checker(10)
