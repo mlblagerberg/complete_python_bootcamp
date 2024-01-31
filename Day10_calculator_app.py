416fc14c77f3bceb7020d1fcd23e38d96f13520b
mlblagerberg <mlblagerberg@gmail.com>
2024-01-31 14:45:45 -0800

    Working through initial attempt on calculator app. Stuck on reinitiating function with output value if user wants to continue calculations.

--- Day10_calculator_app.py
+++ Day10_calculator_app.py
@@ -7,7 +7,40 @@ Author: Madeleine L.
 
 ### Attempt before lesson
 
-val = input("What's the first number? ")
-print(f"+\n-\n*\n/\nPick an operation: ")
-val2 = input("What's the next number? ")
-cont = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+def calculator(val, op, val2):
+    if op in ["+", "-"]:
+        if op == "-":
+            val2 *= -1
+        calc = val + val2
+        return calc
+    elif op in ["*", "/"]:
+        if op == "/":
+            if val2 != 0:
+                val2 = (1/val2)
+            else:
+                print("Error: division by zero. Please try again.")
+        calc = val * val2
+        return calc
+    else:
+        print(f"{op} is not a valid operator. Please choose from the provided list.")
+
+def calc_app(start = 0):
+    if start == 0:
+        val = int(input("What's the first number? "))
+
+    op = input(f"+\n-\n*\n/\nPick an operation: ")
+    val2 = int(input("What's the next number? "))
+    calc = calculator(val, op, val2)
+    cont = input(f"Type 'y' to continue calculating with {calc}, or type 'n' to start a new calculation: ")
+    # print(cont)
+    # print(len(cont))
+    # print(start)
+    if cont.lower() == "y":
+        start += 1
+        val = calc
+        calc_app(start)
+    if cont.lower() == "n":
+        return calc
+            
+# print(calculator(3, "+", 2))
+calc_app()
