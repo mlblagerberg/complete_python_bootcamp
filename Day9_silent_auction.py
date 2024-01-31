5ba1a9dba02b47189344ba9b667e91b12d7c4e67
mlblagerberg <mlblagerberg@gmail.com>
2024-01-30 20:59:21 -0800

    Interactive exercise, on plane so not in typical app.

--- Day9_silent_auction.py
+++ Day9_silent_auction.py
@@ -18,7 +18,7 @@ programming_dictionary = {
 
 # Retrieve list syntax is list[index]
 # Dictionary elements are identified by their key versus index
-print(programming_dictionary["Bug"])
+# print(programming_dictionary["Bug"])
 
 # Dictionary common pitfalls
 # 1. Misspelling keys or attempting to retrieve incorrect key
@@ -26,8 +26,28 @@ print(programming_dictionary["Bug"])
 
 # Adding new entry to dictionary
 programming_dictionary["Loop"] = "The action of doing something over and over again."
-print(programming_dictionary)
+# print(programming_dictionary)
 
 # Create empty dictionary you can also wipe a dictionary with this as well (useful for restartign games and scores)
 empty_dict = {}
 
+# Redefine "Bug" key
+programming_dictionary["Bug"] = "A moth in your computer."
+# print(programming_dictionary)
+
+# How to loop through dictionary
+for key in programming_dictionary:
+    print(key) # returns just the keys
+    print(programming_dictionary[key])
+
+# Program to grade a students test score
+student_scores = {
+    "Harry": 80,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+# Create an empty dictionary called student_grades
+# 91 - 100: outstanding, 81 - 90: exceeds expectations, 71 - 80: acceptable, <=70: fail
