4456738641d21491f77be0abe3f2724f481431b8
mlblagerberg <mlblagerberg@gmail.com>
2024-01-30 20:43:45 -0800

    Creating Day 9 file and initial review of dictionaries.

---
+++ Day9_silent_auction.py
@@ -0,0 +1,23 @@
+"""
+Project: Silent Auction
+Start: January 30th, 2024
+Last touched: January 30th, 2024 
+Author: Madeleine L.
+"""
+
+### Dictionaries and nestings review
+## Dictionaries have two parts a key, and then an associated value or definition of the key.
+## Example given is a table with two columns, key and value
+## Python syntax is {key: value}
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again.",
+                          }
+
+# Retrieve list syntax is list[index]
+# Dictionary elements are identified by their key versus index
+print(programming_dictionary["Bug"])
+
+
