5d9a49cfd21a9bfcab73bb13d45c62e2503041c6
mlblagerberg <mlblagerberg@gmail.com>
2024-02-07 12:15:23 -0800

    Adding comments.

--- Day12_number_guessing.py
+++ Day12_number_guessing.py
@@ -26,6 +26,7 @@ import random
 ## with all caps, (e.g. PI, URL, etc).
 
 ### Number guessing game
+# Function to determine if user has won, lost, or can continue playing.
 def check_guess(num_guess, lives, rand_num):
     if lives == 0 and num_guess == rand_num:
         print(f"You win! The number was {rand_num}.")
@@ -39,36 +40,26 @@ difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 random_number = random.randint(1,100)
 guess = int(input("Make a guess: "))
 
-# print([*difficulty])
+# Conditional to assign lives dependening on difficulty selected. Print error handling if not given valid input.
 if difficulty.lower() == "easy":
     remaining_lives = 10
 elif difficulty.lower == "hard":
     remaining_lives = 5
 else:
-    remaining_lives = 5
-# print(remaining_lives)
+    print(f"{difficulty} is not a valid input. Please try again.")
 
+# Loop to check remaining lives before checking accuracy of guess.
 while remaining_lives > 0:
     remaining_lives -= 1
     if guess == random_number:
-        print("You win!")
         remaining_lives = 0
     elif guess > random_number:
         print("Too high.")
-        # print(f"You have {remaining_lives} attempt(s) to guess the number.")
         guess = int(input("Guess again: "))
     elif guess < random_number:
         print("Too low.")
-        # print(f"You have {remaining_lives} attempt(s) to guess the number.")
         guess = int(input("Guess again: "))
     check_guess(guess, remaining_lives, random_number)
-
-    
-
-# If hard then you have 5 attempts to guess (print this out)
-# If easy you have 10 attempts  
-# Ask for initial guess then tell the user if they are too high or too low and then reduce their remaining number of guesses.
-# Repeat until user guesser or is out of turns.
 
 
 
