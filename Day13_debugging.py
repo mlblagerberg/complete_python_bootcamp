090a3caf8852f063d2c534fd5aea28292ae21edc
mlblagerberg <mlblagerberg@gmail.com>
2024-02-09 11:20:56 -0800

    Most boring debugging ever.

--- Day13_debugging.py
+++ Day13_debugging.py
@@ -4,10 +4,12 @@ Start: February 8th, 2024
 Last touched: February 8th, 2024 
 Author: Madeleine L.
 """
-
+# Code was copied from: https://replit.com/@appbrewery/day-13-start#main.py
 ############DEBUGGING#####################
 
 # # Describe Problem
+# # Problem is that range function does not include end value
+# # and print only occurs when i = 20 which never occurs.
 # def my_function():
 #   for i in range(1, 20):
 #     if i == 20:
@@ -17,35 +19,35 @@ Author: Madeleine L.
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
+# dice_num = randint(0, 5) #(1, 6) Includes both end points
 # print(dice_imgs[dice_num])
 
 # # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year <= 1994:
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
 
 # # Fix the Errors
-# age = input("How old are you?")
+# age = int(input("How old are you? "))
 # if age > 18:
-# print("You can drive at age {age}.")
+#     print(f"You can drive at age {age}.")
 
-# #Print is Your Friend
+# # Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
 
-# #Use a Debugger
+# # Use a Debugger
 # def mutate(a_list):
 #   b_list = []
 #   for item in a_list:
 #     new_item = item * 2
-#   b_list.append(new_item)
+#     b_list.append(new_item)
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
