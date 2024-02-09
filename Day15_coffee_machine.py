5f0dc21e40a6f336654bef54f7369388e215fff5
mlblagerberg <mlblagerberg@gmail.com>
2024-02-09 13:59:38 -0800

    "Starting todo list"

---
+++ Day15_coffee_machine.py
@@ -0,0 +1,14 @@
+"""
+Project: Coffee Machine
+Start: February 9th, 2024
+Last touched: February 9th, 2024 
+Author: Madeleine L.
+"""
+
+# TODO: 1. Ask user what kind of coffee they want. (espresso/latte/cappuccino)
+# TODO: 2. Turn off the coffee machine with "off"
+# TODO: 3. When user enters prompt for report, generate report that shows current resource values:
+#  water: 100ml, milk:50ml, coffee: 76g, and money: $2.5
+# TODO: 4. When user asks for drink check resources before making drink and accepting money. E.g. print
+#  "Sorry there is not enough water."
+
