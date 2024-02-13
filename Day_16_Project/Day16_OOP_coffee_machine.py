774e70cd174f538dc113f8fa14437ea6a403bc65
mlblagerberg <mlblagerberg@gmail.com>
2024-02-13 14:41:19 -0800

    Fixed math for concentric function to work properly. Issue was that I was substracting previous median from next which only works for first case subsequent cases need to remember the new coordinates which done by updating x,y rather than recalculating.

--- Day_16_Project/Day16_OOP_coffee_machine.py
+++ Day_16_Project/Day16_OOP_coffee_machine.py
@@ -15,12 +15,17 @@ timmy.width(3)
 
 
 def concentric_triangles(count_tri, initial_size):
+    previous_median = x = 0
+    previous_size = y = 0
     for i in range(0, count_tri):
         median = (initial_size ** 2 - (initial_size / 2) ** 2) ** (1 / 2)
         # Draw a triangle
-        x = -(initial_size/(2*(i+1)))
-        y = -(median/(3*(i+1)))
-        timmy.setpos(i*x, i*y)
+        if i == 0:
+            timmy.setpos(0, 0)
+        else:
+            x -= (initial_size/2 - previous_size/2)
+            y -= (median/3 - previous_median/3)
+            timmy.setpos(x, y)
         # print(timmy.pos())
         turns = 3
         while turns > 0:
@@ -28,10 +33,13 @@ def concentric_triangles(count_tri, initial_size):
             timmy.forward(initial_size)
             timmy.left(120)
             turns -= 1
+        previous_median = median
+        previous_size = initial_size
         initial_size *= 2
 
 
-concentric_triangles(4,50)
+
+concentric_triangles(5,25)
 my_screen = Screen()
 print(my_screen.canvheight)
 
