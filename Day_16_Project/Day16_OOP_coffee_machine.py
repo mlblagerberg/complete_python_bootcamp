b4fd57ec4b1c0b0d955d9f81b8bf72822d1a26b2
mlblagerberg <mlblagerberg@gmail.com>
2024-02-13 13:43:19 -0800

    Playing around with turtle module.

--- Day_16_Project/Day16_OOP_coffee_machine.py
+++ Day_16_Project/Day16_OOP_coffee_machine.py
@@ -5,7 +5,23 @@ Last touched: February 13th, 2024
 Author: Madeleine L.
 """
 
-import turtle
+from turtle import Turtle, Screen
 
-timmy = turtle.Turtle()
+timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("DeepPink3")
+
+# Draw a trianle
+turns = 3
+while turns > 0:
+    timmy.forward(100)
+    timmy.left(120)
+    turns -= 1
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
+
