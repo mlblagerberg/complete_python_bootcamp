f7a99bae0447241694f39ed779a0d6c44cde50b7
mlblagerberg <mlblagerberg@gmail.com>
2024-02-13 14:23:23 -0800

    Creating a function to draw concentric circles, doign math in head is hard... so isn't quite right yet. Need paper to refine.

--- Day_16_Project/Day16_OOP_coffee_machine.py
+++ Day_16_Project/Day16_OOP_coffee_machine.py
@@ -11,15 +11,27 @@ timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
 timmy.color("DeepPink3")
-
-# Draw a trianle
-turns = 3
-while turns > 0:
-    timmy.forward(100)
-    timmy.left(120)
-    turns -= 1
+timmy.width(3)
 
 
+def concentric_triangles(count_tri, initial_size):
+    for i in range(0, count_tri):
+        median = (initial_size ** 2 - (initial_size / 2) ** 2) ** (1 / 2)
+        # Draw a triangle
+        x = -(initial_size/(2*(i+1)))
+        y = -(median/(3*(i+1)))
+        timmy.setpos(i*x, i*y)
+        # print(timmy.pos())
+        turns = 3
+        while turns > 0:
+            # timmy.width(turns)
+            timmy.forward(initial_size)
+            timmy.left(120)
+            turns -= 1
+        initial_size *= 2
+
+
+concentric_triangles(4,50)
 my_screen = Screen()
 print(my_screen.canvheight)
 
