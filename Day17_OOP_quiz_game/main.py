76f5f26f383e6b28f54534cab6787d13f3e457a5
mlblagerberg <mlblagerberg@gmail.com>
2024-02-17 21:31:14 -0800

    Creating simple class.

--- Day17_OOP_quiz_game/main.py
+++ Day17_OOP_quiz_game/main.py
@@ -12,10 +12,17 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 # create object from class with attribute as arguments
 user_1 = User("001", "mimi")
 user_2 = User("002", "buck")
 
-print(user_1.username)
+print(user_2.followers)
+user_1.follow(user_2)
+print(user_2.followers)
 
