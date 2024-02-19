7a5896f3f5b601ec4f77861adeaa9b33e6b22ece
mlblagerberg <mlblagerberg@gmail.com>
2024-02-18 23:12:47 -0800

    simple initial requirements.

--- Day17_OOP_quiz_game/main.py
+++ Day17_OOP_quiz_game/main.py
@@ -4,5 +4,7 @@ Last touched: February 18th, 2024
 Author: Madeleine L.
 """
 
-
+# If we have a question object what objects would it have?
+# - Would have text and answer
+# - Methods would be to take a guess and check it against an answer
 
