45cfd3c05a87c43917f0051b355de1bd5dafd22d
mlblagerberg <mlblagerberg@gmail.com>
2024-02-20 16:45:22 -0800

    Creating quiz functions within the quizbrain class.

--- Day17_OOP_quiz_game/main.py
+++ Day17_OOP_quiz_game/main.py
@@ -6,13 +6,13 @@ Author: Madeleine L.
 
 from data import question_data
 from question_model import Question
-from quiz_brain import Quiz
+from quiz_brain import QuizBrain
 
 # If we have a question object what objects would it have?
 # - Would have text and answer
 # - Methods would be to take a guess and check it against an answer
 
-# int_quiz = Quiz()
+# int_quiz = QuizBrain()
 # int_quiz.start_quiz()
 
 # create question bank as lecture asks
@@ -24,7 +24,12 @@ for i in range(0, len(question_data)):
     question = Question(text, answer)
     question_bank.append(question)
 
-print(question_bank)
+# print(question_bank[0].text)
+
+quiz = QuizBrain(question_bank)
+
+quiz.get_question()
+quiz.quiz_time()
 
 # my_q = Question("2+3=5", True)
 # print(my_q.text)
--- Day17_OOP_quiz_game/quiz_brain.py
+++ Day17_OOP_quiz_game/quiz_brain.py
@@ -1,21 +1,61 @@
-from data import question_data
-from question_model import Question
+# from data import question_data
+# from question_model import Question
 
 
-class Quiz:
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = question_list
 
-    def __init__(self):
-        self.question = question_data
+    def next_question(self):
+        self.question_number += 1
+        return self.question_number
 
-    def start_quiz(self):
-        start = input("Would you like to play quiz game? 'Y' or 'N' ")
+    def get_question(self):
+        text = self.question_list[self.question_number].text
+        answer = self.question_list[self.question_number].answer
+        return text, answer
+
+    def quiz_time(self):
+        # check start
+        if self.question_number == 0:
+            start = input("Would you like to play quiz game? 'Y' or 'N' ")
+        else:
+            start = input(f"Would you like another question? 'Y' or 'N' ")
         if start == 'Y':
-            question_bank = Question("text", "answer")
-            quiz_question = question_bank.get_question()
-            user_answer = input(quiz_question[0])
-            question_answer = quiz_question[1]
-            return print(user_answer), print(question_answer)
-
-
+            question = self.get_question()
+            question_text = question[0]
+            question_answer = question[1]
+            user_answer = input(f"Q{self.question_number}: {question_text} (True/False)? ")
+            self.next_question()
+            if user_answer == question_answer:
+                self.score += 1
+                # print(question_answer)
+                print(f"You're right! Your score is {self.score}.")
+                self.quiz_time()
+            else:
+                # print(question_answer)
+                print(f"You were wrong. Your score is {self.score}.")
+                self.quiz_time()
         else:
             print("Goodbye!")
+
+
+
+
+#######################################################
+# Below is from working on it before the lesson prompts.
+    # def __init__(self):
+    #     self.question = question_data
+
+    # def start_quiz(self):
+    #     start = input("Would you like to play quiz game? 'Y' or 'N' ")
+    #     if start == 'Y':
+    #         question_bank = Question("text", "answer")
+    #         quiz_question = question_bank.get_question()
+    #         user_answer = input(quiz_question[0])
+    #         question_answer = quiz_question[1]
+    #         return print(user_answer), print(question_answer)
+    #     else:
+    #         print("Goodbye!")
