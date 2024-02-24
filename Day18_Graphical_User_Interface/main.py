abd72ff97ec6fdcd00c05ae5acc3e5e676210d08
mlblagerberg <mlblagerberg@gmail.com>
2024-02-24 12:05:33 -0800

    Minor changes. Mabel screaming.

--- Day18_Graphical_User_Interface/main.py
+++ Day18_Graphical_User_Interface/main.py
@@ -8,20 +8,31 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 tim.shape("arrow")
-tim.color("light green")
+# tim.color("light green")
+tim.color("dark cyan")
+tim.up()
+tim.setposition(x=(-100), y=0)
 
 
 # Draw square
-n = 4
-while n > 0:
-    tim.forward(100)
-    tim.right(90)
-    n -= 1
+# n = 4
+# while n > 0:
+#     tim.forward(100)
+#     tim.right(90)
+#     n -= 1
+#
+# tim.color("dark cyan")
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
 
-tim.color("dark cyan")
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+# Draw dashed line
+for _ in range(50):
+    tim.down()
+    tim.forward(5)
+    tim.up()
+    tim.forward(5)
+
 
 screen = Screen()
 screen.exitonclick()
