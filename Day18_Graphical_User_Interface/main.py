51febc1b3a840c88bedbeb66433bc411902bcd94
mlblagerberg <mlblagerberg@gmail.com>
2024-02-25 21:31:27 -0800

    Adding change_color function to randomize turtle color. Created for loop that creates pattern of shapes increaes the number of sides with each loop.

--- Day18_Graphical_User_Interface/main.py
+++ Day18_Graphical_User_Interface/main.py
@@ -5,15 +5,12 @@ Author: Madeleine L.
 """
 
 from turtle import Turtle, Screen
+from random import random
 
 tim = Turtle()
-tim.shape("arrow")
+tim.shape("turtle")
 # tim.color("light green")
-tim.color("dark cyan")
-tim.up()
-tim.setposition(x=(-250), y=0)
-
-
+# tim.color("deep pink")
 # Draw square
 # n = 4
 # while n > 0:
@@ -27,12 +24,33 @@ tim.setposition(x=(-250), y=0)
 #     tim.right(90)
 
 # Draw dashed line
-for _ in range(50):
-    tim.down()
-    tim.forward(5)
-    tim.up()
-    tim.forward(5)
+# tim.up()
+# tim.setposition(x=(-250), y=0)
+# for _ in range(50):
+#     tim.down()
+#     tim.forward(5)
+#     tim.up()
+#     tim.forward(5)
+
+
+def change_color():
+    r = random()
+    b = random()
+    g = random()
+
+    tim.color(r, b, g)
+
+
+for i in range(0, 10):
+    change_color()
+    side = 3 + i
+    for j in range(0, side):
+        tim.forward(100)
+        angle = 360 / side
+        tim.right(angle)
+        # tim.forward(50)
 
 
 screen = Screen()
+# screen.colormode(255)
 screen.exitonclick()
