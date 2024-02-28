2edb6284ead4f0ea7d2d9b895118f99da1c3792b
mlblagerberg <mlblagerberg@gmail.com>
2024-02-28 14:54:49 -0800

    Etch A sketch.

--- Day19_EtchASketch_TurtleRace/scratch.py
+++ Day19_EtchASketch_TurtleRace/scratch.py
@@ -14,12 +14,16 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
 def turn_left():
-    tim.left(90)
+    tim.left(5)
 
 
 def turn_right():
-    tim.right(90)
+    tim.right(5)
 
 
 def point_north():
@@ -33,7 +37,8 @@ def point_south():
 # Higher order functions: when you pass a function as an input you only use name not () at en because () at end executes
 # function immediately
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="Left", fun=turn_left)
 screen.onkey(key="Right", fun=turn_right)
 screen.onkey(key="Up", fun=point_north)
