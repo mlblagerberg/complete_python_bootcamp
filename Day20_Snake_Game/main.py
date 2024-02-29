1975d393d5c54849f37f1809517ddd1816ec5cde
mlblagerberg <mlblagerberg@gmail.com>
2024-02-29 11:27:08 -0800

    Creating snake class file.

--- Day20_Snake_Game/main.py
+++ Day20_Snake_Game/main.py
@@ -10,6 +10,7 @@ screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 # Todo 1: create snake body
 turtles = []
@@ -21,18 +22,23 @@ for turtle_index in range(0, 3):
     t.setposition(x, 0)
     turtles.append(t)
 
+screen.update()
+
+# turtles[0].setheading(90)
+
 
 # Todo 2: continuous move forward
 def continuous_move_forward():
     move_forward = True
     while move_forward:
-        for turtle in turtles:
-            turtle.forward(5)
-            if turtles[0].pos()[0] + 10 >= 300:
-                move_forward = False
-
-# def face_north():
-#     for turtle in
+        screen.update()
+        for turtle in range(len(turtles)-1, 0, -1):
+            new_x = turtles[turtle - 1].xcor()
+            new_y = turtles[turtle - 1].ycor()
+            turtles[turtle].goto(new_x, new_y)
+        turtles[0].forward(1)
+        if abs(turtles[0].pos()[0] + 10) >= 300 or abs(turtles[0].pos()[1] + 10) >= 300:
+            move_forward = False
 
 
 screen.listen()
---
+++ Day20_Snake_Game/snake.py
@@ -0,0 +1,5 @@
+"""Project: Snake Class for Snake Game
+Start: February 29th, 2024
+Last touched: February 29th, 2024
+Author: Madeleine L.
+"""
