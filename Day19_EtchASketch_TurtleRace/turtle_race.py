a6f10b8cc44d2cff18fecc54436b7de1ab56ebe5
mlblagerberg <mlblagerberg@gmail.com>
2024-02-28 16:52:39 -0800

    Removing event listeners and putting in while loop.

--- Day19_EtchASketch_TurtleRace/turtle_race.py
+++ Day19_EtchASketch_TurtleRace/turtle_race.py
@@ -7,75 +7,48 @@ Author: Madeleine L.
 from turtle import Turtle, Screen
 from random import randint
 screen = Screen()
-screen.colormode(255)
+screen.setup(700, 600)
 screen.listen()
+race_is_on = False
+user_guess = screen.textinput("Make your guess", "Guess which turtle will win: ")
 
-# # Function to get random colors
-# def random_color(t):
-#     r = randint(0, 255)
-#     g = randint(0, 255)
-#     b = randint(0, 255)
-#
-#     t.color(r, g, b)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-
-tim1 = Turtle()
-tim2 = Turtle()
-tim3 = Turtle()
-tim4 = Turtle()
-tim5 = Turtle()
-
-start = Turtle()
-start.hideturtle()
-start.up()
-finish = Turtle()
-finish.hideturtle()
-finish.up()
-
-# # Draw start and finish lines
-# start.setposition(-300, -200)
-# start.setheading(90)
-# start.down()
-# start.forward(500)
-#
-# finish.setposition(300, -200)
-# finish.setheading(90)
-# finish.down()
-# finish.forward(500)
-
-turtles = [tim1, tim2, tim3, tim4, tim5]
-tim1.color("red")
-tim2.color("orange")
-tim3.color("yellow")
-tim4.color("green")
-tim5.color("blue")
-
-y = 150
-for t in turtles:
+y = 200
+turtles = []
+for turtle_index in range(0, 6):
+    t = Turtle(shape="turtle")
     t.up()
-    # random_color(t)
-    t.shapesize(3)
-    t.shape("turtle")
+    t.shapesize(2)
+    t.color(colors[turtle_index])
     t.setposition(-300, y)
     y -= 75
+    turtles.append(t)
+
+if user_guess:
+    race_is_on = True
 
 
-def turtle_race():
-    for turd in turtles:
-        if turd.pos()[0] >= 300:
-            turd.setposition(0, 0)
-        else:
-            step = randint(1, 15)
+# def turtle_race():
+    while race_is_on:
+        for turd in turtles:
+            step = randint(0, 15)
             turd.forward(step)
+            if turd.pos()[0] >= 350:
+                if turd.color()[1] == user_guess.lower():
+                    print(f"You won! The {turd.color()[0]} turtle is the winner!")
+                else:
+                    print(f"You lost. The {turd.color()[0]} turtle is the winner.")
+                race_is_on = False
 
 
 def turtle_reset():
-    y = 150
-    for turd in turtles:
-        turd.setposition(-300, y)
+    y = 200
+    for turdley in turtles:
+        turdley.setposition(-300, y)
         y -= 75
 
 
-screen.onkey(key="space", fun=turtle_race)
+# screen.onkey(key="s", fun=turtle_race)
 screen.onkey(key="r", fun=turtle_reset)
 screen.exitonclick()
