5d33b86b75e958213d48c969aef7acb68c12c0a6
mlblagerberg <mlblagerberg@gmail.com>
2024-02-29 10:35:22 -0800

    Adding race repeat option and cleaning up code.

--- Day19_EtchASketch_TurtleRace/turtle_race.py
+++ Day19_EtchASketch_TurtleRace/turtle_race.py
@@ -8,9 +8,7 @@ from turtle import Turtle, Screen
 from random import randint
 screen = Screen()
 screen.setup(700, 600)
-# screen.listen()
 race_is_on = False
-user_guess = screen.textinput("Make your guess", "Guess which turtle will win: ")
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
@@ -25,30 +23,46 @@ for turtle_index in range(0, 6):
     y -= 75
     turtles.append(t)
 
-if user_guess:
-    race_is_on = True
+
+def race_reset():
+    global race_is_on
+    print("Resetting race...")
+    y_cord = 200
+    for turdley in turtles:
+        turdley.setposition(-300, y_cord)
+        y_cord -= 75
+    race_is_on = False
 
 
-# def turtle_race():
+def turtle_race():
+    global race_is_on
+    user_guess = screen.textinput("Make your guess", "Guess which turtle will win: ")
+    if user_guess:
+        race_is_on = True
+    # winner = []
     while race_is_on:
         for turd in turtles:
             step = randint(0, 15)
             turd.forward(step)
-            if turd.pos()[0] >= 350:
-                if turd.color()[1] == user_guess.lower():
-                    print(f"You won! The {turd.color()[0]} turtle is the winner!")
+            if turd.pos()[0] >= 330:
+                winner = max(turtles, key=lambda x: x.pos()[0])
+                if winner.color()[1] == user_guess.lower():
+                    print(f"You won! The {winner.color()[0]} turtle is the winner!")
                 else:
-                    print(f"You lost. The {turd.color()[0]} turtle is the winner.")
+                    print(f"You lost. The {winner.color()[0]} turtle is the winner.")
                 race_is_on = False
+                break
+        if not race_is_on:
+            play_again = input(f"Do you want to play again? Answer 'yes' or 'no': ")
+            if play_again.lower() == 'yes':
+                race_reset()
+                turtle_race()
+            else:
+                print("Thanks for playing!")
 
 
-# def turtle_reset():
-#     y = 200
-#     for turdley in turtles:
-#         turdley.setposition(-300, y)
-#         y -= 75
-
-
-# screen.onkey(key="s", fun=turtle_race)
-# screen.onkey(key="r", fun=turtle_reset)
+screen.onkey(key="r", fun=turtle_race)
+# screen.onkey(key="space", fun=race_reset)
+screen.listen()
+# screen.getcanvas().bind("<space>", lambda event: race_reset())
 screen.exitonclick()
