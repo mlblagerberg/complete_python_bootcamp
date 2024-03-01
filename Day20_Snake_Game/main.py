17c2570f8f55e01384eb31da8c61742dd691352d
mlblagerberg <mlblagerberg@gmail.com>
2024-03-01 15:34:43 -0800

    Moving snake definition into snake class.

--- Day20_Snake_Game/main.py
+++ Day20_Snake_Game/main.py
@@ -1,10 +1,11 @@
 """Project: Snake Game
 Start: February 29th, 2024
-Last touched: February 29th, 2024
+Last touched: March 1st, 2024
 Author: Madeleine L.
 """
 
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -13,48 +14,23 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-# Todo 1: create snake body
-turtles = []
-for turtle_index in range(0, 3):
-    t = Turtle(shape="square")
-    t.penup()
-    t.color("white")
-    x = -20 * turtle_index
-    t.setposition(x, 0)
-    turtles.append(t)
+snake = Snake()
 
-screen.update()
+move_forward = True
+while move_forward:
+    screen.update()
+    time.sleep(0.1)
 
-# turtles[0].setheading(90)
-
-
-# Todo 2: continuous move forward
-def continuous_move_forward():
-    move_forward = True
-    while move_forward:
-        screen.update()
-        time.sleep(0.1)
-        for turtle in range(len(turtles)-1, 0, -1):
-            new_x = turtles[turtle - 1].xcor()
-            new_y = turtles[turtle - 1].ycor()
-            turtles[turtle].goto(new_x, new_y)
-        turtles[0].forward(20)
-        if abs(turtles[0].pos()[0] + 10) >= 300 or abs(turtles[0].pos()[1] + 10) >= 300:
-            move_forward = False
-
+    snake.move()
 
 screen.listen()
-screen.onkey(key="space", fun=continuous_move_forward)
+screen.onkey(key="space", fun=snake.move())
 # Todo 3: create snake food
 # Todo 4: control snake
 # screen.onkey(key="Up", fun=face_north)
 # Todo 5: detect collision with food
 # Todo 6: create scoreboard
 # Todo 7: detect collision with wall or tail and end game
-
-
-
-
 
 
 
--- Day20_Snake_Game/snake.py
+++ Day20_Snake_Game/snake.py
@@ -1,5 +1,33 @@
 """Project: Snake Class for Snake Game
 Start: February 29th, 2024
-Last touched: February 29th, 2024
+Last touched: March 1st, 2024
 Author: Madeleine L.
 """
+
+from turtle import Turtle, Screen
+
+
+class Snake:
+    def __init__(self):
+        self.length = 3
+        self.turtles = []
+# Todo 1: create snake body
+        for turtle_index in range(0, self.length):
+            t = Turtle(shape="square")
+            t.penup()
+            t.color("white")
+            x = -20 * turtle_index
+            t.setposition(x, 0)
+            self.turtles.append(t)
+
+
+# turtles[0].setheading(90)
+    # Todo 2: continuous move forward
+    def move(self):
+        for turtle in range(len(self.turtles)-1, 0, -1):
+            new_x = self.turtles[turtle - 1].xcor()
+            new_y = self.turtles[turtle - 1].ycor()
+            self.turtles[turtle].goto(new_x, new_y)
+        self.turtles[0].forward(20)
+        if abs(self.turtles[0].pos()[0] + 10) >= 300 or abs(self.turtles[0].pos()[1] + 10) >= 300:
+            exit()
