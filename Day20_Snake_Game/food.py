e21704ae59c45c1f51454b9ebab9b31682a67f63
mlblagerberg <mlblagerberg@gmail.com>
2024-03-02 14:51:19 -0800

    Creating food class.

---
+++ Day20_Snake_Game/food.py
@@ -0,0 +1,15 @@
+"""Project: Food Class for Snake Game
+Start: March 2nd, 2024
+Last touched: March 2nd, 2024
+Author: Madeleine L.
+"""
+
+
+from turtle import Turtle
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
