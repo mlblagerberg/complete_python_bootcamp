834e108431377f485f002d9d136dbb6602492ee4
mlblagerberg <mlblagerberg@gmail.com>
2024-03-02 15:19:50 -0800

    Adding ability for snake to 'eat' food and food to disapear and create new food.

--- Day20_Snake_Game/food.py
+++ Day20_Snake_Game/food.py
@@ -6,10 +6,20 @@ Author: Madeleine L.
 
 
 from turtle import Turtle
+from random import randint
 
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("orange")
+        self.speed("fastest")
+        x = randint(-280, 280)
+        y = randint(-280, 280)
+        self.goto(x, y)
+
 
--- Day20_Snake_Game/main.py
+++ Day20_Snake_Game/main.py
@@ -1,11 +1,12 @@
 """Project: Snake Game
 Start: February 29th, 2024
-Last touched: March 1st, 2024
+Last touched: March 2nd, 2024
 Author: Madeleine L.
 """
 
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -15,6 +16,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 screen.listen()
 screen.onkey(key="Up", fun=snake.north)
 screen.onkey(key="Down", fun=snake.south)
@@ -28,10 +30,16 @@ while move_forward:
 
     snake.move()
 
-# Todo 3: create snake food
-# Todo 4: control snake
-# screen.onkey(key="Up", fun=face_north)
-# Todo 5: detect collision with food
+    if abs(snake.head.distance(food)) < 15:
+        food.reset()
+        food = Food()
+
+
+
+# # Todo 5: detect collision with food
+# if snake.head.pos() == food.pos():
+#     food = Food()
+
 # Todo 6: create scoreboard
 # Todo 7: detect collision with wall or tail and end game
 
