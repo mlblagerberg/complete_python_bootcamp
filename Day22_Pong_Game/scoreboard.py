0a5d7d2fb816cbb0184bf7e8fdeff540b6024f32
mlblagerberg <mlblagerberg@gmail.com>
2024-03-10 20:17:38 -0700

    Fixed tracer issues with time module and correct placement of screen.update(). Now the issue is that te score isn't updating for every paddle hit.

--- Day22_Pong_Game/scoreboard.py
+++ Day22_Pong_Game/scoreboard.py
@@ -9,17 +9,17 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
 
-    def __init__(self) #, x_cord, y_cord):
+    def __init__(self, x_cord, y_cord):
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
-        # self.goto(x_cord, y_cord)
-        # self.score = 0
-        # self.write(f"{self.score}", align="center", font=("Courier", 80, "normal"))
+        self.goto(x_cord, y_cord)
+        self.score = 0
+        # self.write(f"{self.score}", align="center", font=("Courier", 70, "normal"))
 
     def update_score(self):
-        self.score += 1
         self.clear()
-        self.write(f"{self.score}", align="center", font=("Courier", 80, "normal"))
+        self.score += 1
+        self.write(f"{self.score}", align="center", font=("Courier", 70, "normal"))
 
