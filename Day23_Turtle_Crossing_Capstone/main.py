694f533cb62934e405480bb72ccc3b61766944fe
mlblagerberg <mlblagerberg@gmail.com>
2024-03-11 23:16:42 -0700

    Creating files and todo list before lesson on turtle crossy roads capstone.

---
+++ Day23_Turtle_Crossing_Capstone/main.py
@@ -0,0 +1,25 @@
+"""Project: Main File for Turtle Crossing Capstone
+Start: March 11th, 2024
+Last touched: March 11th, 2024
+Author: Madeleine L.
+"""
+
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.bgcolor("grey")
+screen.setup(width=800, height=600)
+
+# TODO: Create grey background screen with dotted lines for lanes
+# TODO: Place Turtle at the bottom of the screen and make it move forward by a jump amount with arrow keys
+# TODO: Use random module to create cars to drive across with random speeds in random lanes
+# TODO: Create scoreboard to score how far the turtle has traveled without getting hit
+# TODO: Create coins for game that add to the score and obstacles that cannot be walked over by the turtle
+
+
+
+
+
+
+
+screen.exitonclick()
