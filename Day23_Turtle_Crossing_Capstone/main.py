d6b46ae54451677db74e62b50faaa688fcb0b566
mlblagerberg <mlblagerberg@gmail.com>
2024-03-19 13:33:14 -0700

    Adjusting the number of cars created when a car crosses the midline so that this number increases dependent on player level.

--- Day23_Turtle_Crossing_Capstone/main.py
+++ Day23_Turtle_Crossing_Capstone/main.py
@@ -1,6 +1,6 @@
 """Project: Main File for Turtle Crossing Capstone
 Start: March 11th, 2024
-Last touched: March 15th, 2024
+Last touched: March 19th, 2024
 Author: Madeleine L.
 """
 
@@ -47,30 +47,29 @@ while game_is_on:
         player.next_level()
         scoreboard.increase_level()
     for car in car_list:
-        # Add one car to the list every time a car crosse half way point.
+        # Depending on player's current level X, add ceiling of X/2 cars to the list every time a car crosses the
+        # halfway point.
         if car.xcor() == 0:
-            car_list.append(Car(screen_width=WIDTH, screen_height=HEIGHT))
+            for _ in range(round(scoreboard.level/2)):
+                car_list.append(Car(screen_width=WIDTH, screen_height=HEIGHT))
         if abs(player.xcor() - car.xcor()) < 20:  # and player.xcor() < car.xcor():
             if abs(player.ycor() - car.ycor()) < 20:  # Add proces here to restart game
                 scoreboard.game_over()
                 player.next_level()
                 player.setheading(90)
-                # car_manager = CarManager(screen_width=WIDTH, screen_height=HEIGHT)
-                # car_manager.create_cars()
-                # car_list = car_manager.car_list
-                # game_is_on = False
     if len(car_list) < 10:
         next_car_manager = CarManager(screen_width=WIDTH, screen_height=HEIGHT)
         next_car_manager.create_cars()
         for new_car in next_car_manager.car_list:
             car_list.append(new_car)
 
-# TODO: Define collisions in game with cars and go back to level 1
-# TODO: Create scoreboard to score how far the turtle has traveled without getting hit
+# TODO: Create scoreboard method to score how far the turtle has traveled without getting hit
 # TODO: Create coins for game that add to the score and obstacles that cannot be walked over by the turtle
-# TODO: Create game levels so when turtle reaches te top of the screen increase car speed and move turtle back down to
+# TODO: Create game levels so when turtle reaches the top of the screen increase car speed and move turtle back down to
 #  bottom of the screen
 # TODO: Increase number of single cars that spawn depending on the level the player is on.
+# TODO: Create proper scoring method which adds 1 for every lane you reach without collision
+
 
 
 
