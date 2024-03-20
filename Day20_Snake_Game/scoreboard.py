6494aa8f47ea29a1cb91be06de8f988692e73f15
mlblagerberg <mlblagerberg@gmail.com>
2024-03-20 12:48:33 -0700

    Writing high score to a txt file to keep track across games.

--- Day20_Snake_Game/scoreboard.py
+++ Day20_Snake_Game/scoreboard.py
@@ -1,12 +1,15 @@
 """Project: Scoreboard Class for Snake Game
 Start: March 2nd, 2024
-Last touched: March 19th, 2024
+Last touched: March 20th, 2024
 Author: Madeleine L.
 """
+from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 20, "normal")
 
-from turtle import Turtle
+with open("high_score_data.txt", mode="r") as file:
+    high_score = int(file.read())
 
 
 class Score(Turtle):
@@ -17,7 +20,7 @@ class Score(Turtle):
         self.color("white")
         self.hideturtle()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-        self.high_score = 0
+        self.high_score = high_score
 
     def update_score(self):
         self.score += 1
@@ -29,9 +32,10 @@ class Score(Turtle):
             self.clear()
             self.high_score = self.score
             self.write(f"New high score! {self.high_score}", align=ALIGNMENT, font=FONT)
-            # self.goto(0, 0)
             self.score = 0
+            with open("high_score_data.txt", mode="w") as update_file:
+                update_file.write(str(self.high_score))
         else:
             self.clear()
-            # self.goto(0, 0)
+            self.score = 0
             self.write("Game Over", align=ALIGNMENT, font=FONT)
