e53584c54531c0bdef096ab9c4dfc0a0a6c984c8
mlblagerberg <mlblagerberg@gmail.com>
2024-03-25 14:27:50 -0700

    Simple find replace in txt practice.

--- Day24_files_directories_paths/main.py
+++ Day24_files_directories_paths/main.py
@@ -9,6 +9,9 @@ Author: Madeleine L.
 #     print(contents)
 
 
-with open("my_file.txt", mode="a") as file:  # "a" stands for append, "w" would overwrite everything in file. Default
-    # mode is to "r" read.
-    file.write("This is my random addition.")
+# with open("my_file.txt", mode="a") as file:  # "a" stands for append, "w" would overwrite everything in file. Default
+#     # mode is to "r" read.
+#     file.write("This is my random addition.")
+
+# TODO: For each name in names.txt replace the [name] in the started letter and save letter to output directory
+
