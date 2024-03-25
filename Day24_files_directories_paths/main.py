32a5076f935215410d64a5ebedd8e58b029ad56c
mlblagerberg <mlblagerberg@gmail.com>
2024-03-25 14:56:15 -0700

    Looping through names and replacing [name] in letter.

--- Day24_files_directories_paths/main.py
+++ Day24_files_directories_paths/main.py
@@ -4,6 +4,7 @@ Last touched: March 20th, 2024
 Author: Madeleine L.
 """
 
+import os
 # with open("my_file.txt") as file:
 #     contents = file.read()
 #     print(contents)
@@ -14,4 +15,30 @@ Author: Madeleine L.
 #     file.write("This is my random addition.")
 
 # TODO: For each name in names.txt replace the [name] in the started letter and save letter to output directory
+# Extract names into list to loop over later
+names_list = []
+with open(os.path.expanduser(
+        "~/GitHub/complete_python_bootcamp/Day24_files_directories_paths/Input/Addressee/names.txt")) as names:
+    for name in names.readlines():
+        # print(name.strip())  # strip removes new line \n from end of each name
+        names_list.append(name.strip())
+
+# print(names_list)
+
+# Store letter as string and then replace [name] with names. Saving strings to unique objects
+with open(os.path.expanduser(
+        "~/GitHub/complete_python_bootcamp/Day24_files_directories_paths/Input/Letters/starting_letter.txt"), mode="r"
+) as letter:
+    input_letter = letter.read()
+    for name in names_list:
+        separated_name = name.split()
+        file_name = ""
+        for i in separated_name:
+            file_name += f"{i}_"
+        file_name += f"final_letter.txt"
+
+        output_letter = input_letter.replace("[name]", f"{name}")
+
+        # print(output_letter)
+
 
