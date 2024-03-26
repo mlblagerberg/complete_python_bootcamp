269710159952a142fc7cd636b94b4f0a1c6cf293
mlblagerberg <mlblagerberg@gmail.com>
2024-03-26 09:04:24 -0700

    Minor edits.

--- Day24_files_directories_paths/main.py
+++ Day24_files_directories_paths/main.py
@@ -1,6 +1,6 @@
 """Project: Working with files
 Start: March 20th, 2024
-Last touched: March 20th, 2024
+Last touched: March 26th, 2024
 Author: Madeleine L.
 """
 
@@ -9,12 +9,6 @@ import os
 #     contents = file.read()
 #     print(contents)
 
-
-# with open("my_file.txt", mode="a") as file:  # "a" stands for append, "w" would overwrite everything in file. Default
-#     # mode is to "r" read.
-#     file.write("This is my random addition.")
-
-# TODO: For each name in names.txt replace the [name] in the started letter and save letter to output directory
 # Extract names into list to loop over later
 names_list = []
 with open(os.path.expanduser(
