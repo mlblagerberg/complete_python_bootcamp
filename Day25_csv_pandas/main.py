5bb13da46f0a5b6b8808784e141df5ed7301b275
mlblagerberg <mlblagerberg@gmail.com>
2024-04-01 08:20:56 -1000

    Minor edits.

--- Day25_csv_pandas/main.py
+++ Day25_csv_pandas/main.py
@@ -7,22 +7,23 @@ Author: Madeleine L.
 import csv
 import pandas as pd
 
-weather_data_clean = []
-with open("weather_data.csv") as data:
-    weather_data = data.readlines()
-    for entry in weather_data:
-        clean_entry = entry.strip()
-        weather_data_clean.append(clean_entry)
-    print(weather_data_clean)
-
-with open("weather_data.csv") as data:
-    weather_data = csv.reader(data)
-    print(weather_data)
-    temps = []
-    for row in weather_data:
-        if row[1] != "temp":
-            temps.append(int(row[1]))
-    print(temps)
+# weather_data_clean = []
+# with open("weather_data.csv") as data:
+#     weather_data = data.readlines()
+#     for entry in weather_data:
+#         clean_entry = entry.strip()
+#         weather_data_clean.append(clean_entry)
+#     print(weather_data_clean)
+#
+# with open("weather_data.csv") as data:
+#     weather_data = csv.reader(data)
+#     print(weather_data)
+#     temps = []
+#     for row in weather_data:
+#         if row[1] != "temp":
+#             temps.append(int(row[1]))
+#     print(temps)
 
 weather_data = pd.read_csv("weather_data.csv")
-print(weather_data)
+print(weather_data["temp"])
+
