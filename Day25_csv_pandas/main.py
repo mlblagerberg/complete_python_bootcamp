1bd80bc15801405db8248ebe6aca9ac01b1b360f
mlblagerberg <mlblagerberg@gmail.com>
2024-04-01 08:00:12 -1000

    Working with csv module.

--- Day25_csv_pandas/main.py
+++ Day25_csv_pandas/main.py
@@ -4,6 +4,8 @@ Last touched: March 29th, 2024
 Author: Madeleine L.
 """
 
+import csv
+
 weather_data_clean = []
 with open("weather_data.csv") as data:
     weather_data = data.readlines()
@@ -11,3 +13,13 @@ with open("weather_data.csv") as data:
         clean_entry = entry.strip()
         weather_data_clean.append(clean_entry)
     print(weather_data_clean)
+
+with open("weather_data.csv") as data:
+    weather_data = csv.reader(data)
+    print(weather_data)
+    temps = []
+    for row in weather_data:
+        temp = row[1]
+        temps.append(temp)
+    print(temps)
+    # print(type(weather_data))
