82f3504d84779392e7624ce3c58f297cb2828c91
mlblagerberg <mlblagerberg@gmail.com>
2024-04-19 16:24:37 -0700

    Adding files for NATO alpha project.

---
+++ Day26_NATO_Alpha_Project/main.py
@@ -0,0 +1,12 @@
+"""Project: NATO Alpha Project
+Start: April 19th, 2024
+Last touched: April 19th, 2024
+Author: Madeleine L.
+"""
+
+# List Comprehension: new_list = [new_item for item in list]
+
+numbers = [1, 2, 3]
+
+new_list = [n + 1 for n in numbers]
+print(new_list)
