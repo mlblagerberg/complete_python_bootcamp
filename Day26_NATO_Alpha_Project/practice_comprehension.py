408e6bb86644682a5a02ef13752b1640dab06531
mlblagerberg <mlblagerberg@gmail.com>
2024-04-23 19:17:47 -0700

    Moving comprehension practice to seperate file.

---
+++ Day26_NATO_Alpha_Project/practice_comprehension.py
@@ -0,0 +1,33 @@
+"""Project: Practice Comprehension
+Start: April 23rd, 2024
+Last touched: April 23rd, 2024
+Author: Madeleine L.
+"""
+
+from random import randint
+# # List Comprehension: new_list = [new_item for item in list (can add if conditional at end)]
+#
+# numbers = [1, 2, 3]
+#
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+#
+# names = ["Alex", "Beth", "Johnny", "Travis", "Steph"]
+#
+# short_names = [name for name in names if len(name) < 5]
+# print(short_names)
+#
+# cap_names = [name.upper() for name in names if len(name) >= 5]
+# print(cap_names)
+
+# Dictionary Comprehension: new_dict = {new_key:new_value for (key,value) in dict.items() (or list) (if) }
+names = ["Alex", "Beth", "Johnny", "Travis", "Steph", "Bryan", "Gregory"]
+
+student_scores = {name: randint(0, 100) for name in names}
+
+# print(student_scores)
+# print(student_scores["Alex"])
+passed_students = {student: grade for (student, grade) in student_scores.items() if student_scores[student] > 60}
+
+print(student_scores)
+print(passed_students)
