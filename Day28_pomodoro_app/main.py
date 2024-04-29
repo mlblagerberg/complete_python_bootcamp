91ee1dcdae8ff9ecc2a55aefc943cc277d153819
mlblagerberg <mlblagerberg@gmail.com>
2024-04-29 14:04:30 -0700

    Start to Pomodoro app.

---
+++ Day28_pomodoro_app/main.py
@@ -0,0 +1,22 @@
+"""Project: Pomodoro App
+Start: April 29th, 2024
+Last touched: April 29th, 2024
+Author: Madeleine L.
+"""
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
