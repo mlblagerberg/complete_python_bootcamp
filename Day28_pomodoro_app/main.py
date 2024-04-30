9138f076f610c58d337ef059e21de1eb2109603a
mlblagerberg <mlblagerberg@gmail.com>
2024-04-30 12:52:33 -0700

    Functioning pomodoro app.

--- Day28_pomodoro_app/main.py
+++ Day28_pomodoro_app/main.py
@@ -5,7 +5,7 @@ Author: Madeleine L.
 """
 
 from tkinter import *
-import time
+from math import floor
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,26 +15,53 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
-    pass
+    global reps
+    reps = 0
+    timer_label.config(text="Timer")
+    check.config(text="")
+    window.after_cancel(timer)
+    timer_time = "00:00"
+    canvas.itemconfig(timer_text, text=timer_time)
+    return reps
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
-    pass
+    global reps
+    reps += 1
+    if (reps % 8) == 0:
+        count_down(LONG_BREAK_MIN * 1)  # 20 seconds
+        timer_label.config(text="Break", fg=RED)
+    elif (reps % 2) == 0:
+        count_down(SHORT_BREAK_MIN * 1)  # 5 seconds
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(WORK_MIN * 1)  # 25 seconds
+        timer_label.config(text="Work", fg=GREEN)
+    return reps
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(timer_length):
     if timer_length > 0:
         mins, secs = divmod(timer_length, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        canvas.itemconfig(timer_text, text=timer)
-        window.after(1000, count_down, timer_length - 1)
+        timer_time = '{:02d}:{:02d}'.format(mins, secs)
+        canvas.itemconfig(timer_text, text=timer_time)
+        global timer
+        timer = window.after(1000, count_down, timer_length - 1)
+    else:
+        check_text = ""
+        start_timer()
+        pomodoro = floor(reps / 2)
+        for _ in range(0, pomodoro):
+            check_text += "✔"
+        check.config(text=check_text)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,7 +78,7 @@ reset_button = Button(text="Reset", bg=YELLOW, highlightbackground=YELLOW, comma
 start_button.grid(column=0, row=2)
 reset_button.grid(column=2, row=2)
 # TODO: Check mark icons for each completed Pomodoro
-check = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+check = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
 check.grid(column=1, row=3)
 # TODO: Update 00:00 with countdown
 
