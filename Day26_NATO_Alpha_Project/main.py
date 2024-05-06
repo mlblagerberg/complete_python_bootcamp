ef8815014139839062d75a75f502573a331c716a
mlblagerberg <mlblagerberg@gmail.com>
2024-05-06 09:36:32 -0700

    Adding try/catch for nato alpha project.

--- Day26_NATO_Alpha_Project/main.py
+++ Day26_NATO_Alpha_Project/main.py
@@ -1,6 +1,6 @@
 """Project: NATO Alpha Project
 Start: April 19th, 2024
-Last touched: April 24th, 2024
+Last touched: May 6th, 2024
 Author: Madeleine L.
 """
 
@@ -12,10 +12,20 @@ nato_alpha = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_alpha.iterrows()}
 print(nato_dict)
 
+
 # Create a list of the phonetic code words from a word that the user inputs.
-user_phrase = input("Please input a word or phrase: ").upper()
-word_list = user_phrase.split()
-for user_word in word_list:
-    word_letters = [letter for letter in user_word]
-    nato_code = [nato_dict[letter] for letter in word_letters]
-    print(f"{user_word}: {nato_code}")
+def nato_alpha():
+    user_phrase = input("Please input a word or phrase: ").upper()
+    word_list = user_phrase.split()
+    for user_word in word_list:
+        word_letters = [letter for letter in user_word]
+        try:
+            nato_code = [nato_dict[letter] for letter in word_letters]
+        except KeyError:
+            print("Only letters in the alphabet are excepted.")
+            nato_alpha()
+        else:
+            print(f"{user_word}: {nato_code}")
+
+
+nato_alpha()
