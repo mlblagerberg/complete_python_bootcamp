353fe9ced04d5e4a8519ca372e89b372c765ba55
mlblagerberg <mlblagerberg@gmail.com>
2024-05-16 16:21:03 -0700

    Starting tkinter UI.

--- Day31_Korean_Flashcards/main.py
+++ Day31_Korean_Flashcards/main.py
@@ -6,6 +6,7 @@ Author: Madeleine L.
 
 
 import pandas as pd
+from tkinter import *
 
 # words = pd.read_excel("한국어 학습용 어휘 목록.xls")
 words = pd.read_csv("한국어 학습용 어휘 목록 - 한국어학습용어휘등급표 원어 정보 정리 쿼리.csv")
@@ -13,3 +14,7 @@ sorted_words = words.sort_values("순위 (ranking)")
 cleaned_words = sorted_words.drop(labels=["품사 (Part of speech)", "단어 (word)", "풀이 (Explanation)", "등급 (Rating)",
                                           "suffix number"], axis=1)
 print(cleaned_words.head())
+window = Tk()
+window.title("Flashy")
+x_image = PhotoImage()
+check_image = PhotoImage
