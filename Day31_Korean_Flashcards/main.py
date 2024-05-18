70b1cec449a6fb6f15259636650ceed8ad675f43
mlblagerberg <mlblagerberg@gmail.com>
2024-05-17 18:16:38 -0700

    Converting df into dictionary for word pairing.

--- Day31_Korean_Flashcards/main.py
+++ Day31_Korean_Flashcards/main.py
@@ -19,10 +19,12 @@ IMAGE_PATH = "~/GitHub/complete_python_bootcamp/Day31_Korean_Flashcards/images"
 # words = pd.read_excel("한국어 학습용 어휘 목록.xls")
 words = pd.read_csv("한국어 학습용 어휘 목록 - 한국어학습용어휘등급표 원어 정보 정리 쿼리.csv")
 sorted_words = words.sort_values("순위 (ranking)")
-cleaned_words = sorted_words.drop(labels=["품사 (Part of speech)", "단어 (word)", "풀이 (Explanation)", "등급 (Rating)",
+cleaned_words = sorted_words.drop(labels=["순위 (ranking)", "품사 (Part of speech)", "단어 (word)", "풀이 (Explanation)", "등급 (Rating)",
                                           "suffix number"], axis=1)
 # print(cleaned_words.head(20))
+word_dict = cleaned_words.to_dict(orient="records")
 print(choice(cleaned_words["단어 (word) (cleaned)"]))
+print(word_dict[0])
 # -------------------------------------- Button functionality ----------------------------------------- #
 
 
