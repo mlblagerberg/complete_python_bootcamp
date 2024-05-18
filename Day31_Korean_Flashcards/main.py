5f9aabc08ef50f06d3d90d60f561b7ee15187377
mlblagerberg <mlblagerberg@gmail.com>
2024-05-17 18:11:34 -0700

    Adding button functionality. Need to fix outlines.

--- Day31_Korean_Flashcards/main.py
+++ Day31_Korean_Flashcards/main.py
@@ -9,6 +9,7 @@ Author: Madeleine L.
 
 import pandas as pd
 from tkinter import *
+from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 IMAGE_PATH = "~/GitHub/complete_python_bootcamp/Day31_Korean_Flashcards/images"
@@ -21,15 +22,22 @@ sorted_words = words.sort_values("순위 (ranking)")
 cleaned_words = sorted_words.drop(labels=["품사 (Part of speech)", "단어 (word)", "풀이 (Explanation)", "등급 (Rating)",
                                           "suffix number"], axis=1)
 # print(cleaned_words.head(20))
+print(choice(cleaned_words["단어 (word) (cleaned)"]))
 # -------------------------------------- Button functionality ----------------------------------------- #
 
 
 def incorrect_next():
-    pass
+    random_word = choice(cleaned_words["단어 (word) (cleaned)"])
+    word_label.config(text=f"{random_word}")
+    word_label.place(x=375, y=263)
+    # return random_word
 
 
 def correct_next():
-    pass
+    random_word = choice(cleaned_words["단어 (word) (cleaned)"])
+    word_label.config(text=f"{random_word}")
+    word_label.place(x=375, y=263)
+    # return random_word
 
 # -------------------------------------- UI SETUP ----------------------------------------- #
 
@@ -45,22 +53,25 @@ x_image = PhotoImage(file=f"{IMAGE_PATH}/wrong.png")
 check_image = PhotoImage(file=f"{IMAGE_PATH}/right.png")
 card_back = PhotoImage(file=f"{IMAGE_PATH}/card_back.png")
 card_front = PhotoImage(file=f"{IMAGE_PATH}/card_front.png")
-canvas.create_image(225, 650, image=x_image)
-canvas.create_image(675, 650, image=check_image)
+# canvas.create_image(225, 650, image=x_image)
+# canvas.create_image(675, 650, image=check_image)
 canvas.create_image(450, 300, image=card_back)
 canvas.create_image(450, 300, image=card_front)
 canvas.pack()
 
-# Create titles for words
-title_label = Label(text="Title", bg="white", font=("Ariel", 40, "italic"))
-word_label = Label(text="Word", bg="white", font=("Ariel", 60, "bold"))
-title_label.place(x=400, y=150)
-word_label.place(x=355, y=263)
+# Create titles for words and start of practice
+random_start = choice(cleaned_words["단어 (word) (cleaned)"])  # Get random word to start with
+title_label = Label(text="Korean", bg="white", font=("Ariel", 40, "italic"))
+word_label = Label(text=f"{random_start}", bg="white", font=("Ariel", 60, "bold"))
+title_label.place(x=375, y=150)
+word_label.place(x=375, y=263)
 
 # Create buttons
 
-wrong = Button(window, image=x_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=incorrect_next)
-correct = Button(window, image=check_image, bg=BACKGROUND_COLOR, highlightthickness=0, command=correct_next)
+wrong = Button(image=x_image, bg=BACKGROUND_COLOR, highlightcolor=BACKGROUND_COLOR, highlightthickness=0, command=incorrect_next)
+correct = Button(image=check_image, bg=BACKGROUND_COLOR, highlightcolor=BACKGROUND_COLOR, highlightthickness=0, command=correct_next)
+wrong.place(x=175, y=600)
+correct.place(x=625, y=600)
 
 
 window.mainloop()
