e5a408606c56a01cc9257f3e57e83c79e0aa3198
mlblagerberg <mlblagerberg@gmail.com>
2024-05-18 12:26:12 -0700

    Cleaning up code.

--- Day31_Korean_Flashcards/main.py
+++ Day31_Korean_Flashcards/main.py
@@ -10,6 +10,7 @@ Author: Madeleine L.
 import pandas as pd
 from tkinter import *
 from random import choice
+from time import sleep
 
 BACKGROUND_COLOR = "#B1DDC6"
 IMAGE_PATH = "~/GitHub/complete_python_bootcamp/Day31_Korean_Flashcards/images"
@@ -21,29 +22,20 @@ words = pd.read_csv("한국어 학습용 어휘 목록 - 한국어학습용어�
 sorted_words = words.sort_values("순위 (ranking)")
 cleaned_words = sorted_words.drop(labels=["순위 (ranking)", "품사 (Part of speech)", "단어 (word)", "풀이 (Explanation)", "등급 (Rating)",
                                           "suffix number"], axis=1)
-# print(cleaned_words.head(20))
 word_dict = cleaned_words.to_dict(orient="records")
-print(choice(cleaned_words["단어 (word) (cleaned)"]))
-print(word_dict[0])
+print(choice(word_dict))
 # -------------------------------------- Button functionality ----------------------------------------- #
 
 
-def incorrect_next():
-    random_word = choice(cleaned_words["단어 (word) (cleaned)"])
-    word_label.config(text=f"{random_word}")
+def next_card():
+    random_word = choice(word_dict)
+    word = random_word["단어 (word) (cleaned)"]
+    word_label.config(text=f"{word}")
     word_label.place(x=375, y=263)
     # return random_word
 
-
-def correct_next():
-    random_word = choice(cleaned_words["단어 (word) (cleaned)"])
-    word_label.config(text=f"{random_word}")
-    word_label.place(x=375, y=263)
-    # return random_word
 
 # -------------------------------------- UI SETUP ----------------------------------------- #
-
-
 # Create UI window
 window = Tk()
 window.title("Flashy")
@@ -55,25 +47,26 @@ x_image = PhotoImage(file=f"{IMAGE_PATH}/wrong.png")
 check_image = PhotoImage(file=f"{IMAGE_PATH}/right.png")
 card_back = PhotoImage(file=f"{IMAGE_PATH}/card_back.png")
 card_front = PhotoImage(file=f"{IMAGE_PATH}/card_front.png")
-# canvas.create_image(225, 650, image=x_image)
-# canvas.create_image(675, 650, image=check_image)
 canvas.create_image(450, 300, image=card_back)
 canvas.create_image(450, 300, image=card_front)
 canvas.pack()
 
 # Create titles for words and start of practice
-random_start = choice(cleaned_words["단어 (word) (cleaned)"])  # Get random word to start with
 title_label = Label(text="Korean", bg="white", font=("Ariel", 40, "italic"))
-word_label = Label(text=f"{random_start}", bg="white", font=("Ariel", 60, "bold"))
+word_label = Label(text="", bg="white", font=("Ariel", 60, "bold"))
 title_label.place(x=375, y=150)
 word_label.place(x=375, y=263)
 
 # Create buttons
+wrong = Button(image=x_image, bg=BACKGROUND_COLOR, highlightcolor=BACKGROUND_COLOR, highlightthickness=0,
+               command=next_card)
+correct = Button(image=check_image, bg=BACKGROUND_COLOR, highlightcolor=BACKGROUND_COLOR, highlightthickness=0,
+                 command=next_card)
 
-wrong = Button(image=x_image, bg=BACKGROUND_COLOR, highlightcolor=BACKGROUND_COLOR, highlightthickness=0, command=incorrect_next)
-correct = Button(image=check_image, bg=BACKGROUND_COLOR, highlightcolor=BACKGROUND_COLOR, highlightthickness=0, command=correct_next)
+
 wrong.place(x=175, y=600)
 correct.place(x=625, y=600)
 
+next_card()
 
 window.mainloop()
