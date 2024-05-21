81bcb6feae81782fa7e6ebb8492a0b0f6fcf9540
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 12:28:16 -0700

    API access practice.

---
+++ Day33_APIs/api_call_practice.py
@@ -0,0 +1,31 @@
+"""Project: APIs (ISS Tracking Example)
+Start: May 21st, 2024
+Last touched: May 21st, 2024
+Author: Madeleine L.
+"""
+# Respources: HTTP Status Code Glossary https://www.webfx.com/web-development/glossary/http-status-codes/
+
+# Application Program Interfaces (APIs): is a set of commands, protocols, and objects that programmers can use to create
+# software or interact with an external system
+
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response)  # Default is to get the response code
+# Response Codes Review -
+# 1XX: Hold on, something is happening
+# 2XX: Here you go, everything worked as expected
+# 3XX: You don't have permission to get this
+# 4XX: You screwed up. What you are looking for does not exist
+# 5XX: I screwed up (server is down or some other access issue on the websites side of things.)
+# print(response.status_code)
+response.raise_for_status()
+data = response.json()
+latitude = data["iss_position"]["latitude"]
+longitude = data["iss_position"]["longitude"]
+lat_long = [latitude, longitude]
+print(lat_long)
+
+
+
+
