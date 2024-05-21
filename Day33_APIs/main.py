25268c1785da719646d23a537b3dda0daa600f64
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 15:17:15 -0700

    Added while statement to check continuously if ISS is overhead. Will move this to log.

--- Day33_APIs/main.py
+++ Day33_APIs/main.py
@@ -5,6 +5,7 @@ Author: Madeleine L.
 """
 import requests
 from datetime import datetime, timezone
+import time
 import math
 
 LAT = 47.037872
@@ -17,42 +18,54 @@ LONG = -122.900696
 # If the ISS is close to my current position and it is dark out
 # Run code every 60 seconds then send an email
 
-# API input parameters
-parameters = {
-    "lat": LAT,
-    "lng": LONG,
-    "formatted": 0,
- }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise_hour = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset_hour = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-hour_now = datetime.now(timezone.utc)
+def is_night():
+    # API input parameters
+    parameters = {
+        "lat": LAT,
+        "lng": LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise_hour = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset_hour = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    hour_now = datetime.now(timezone.utc)
+    if sunset_hour <= hour_now.hour <= sunrise_hour:
+        return True
 
 # # Estimate visible sky
 # horizon_distance = (2*EARTH_RADIUS*HEIGHT) ** (1 / 2)
 # visible_sky = horizon_distance*PERCENT_SKY
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-latitude = float(data["iss_position"]["latitude"])
-longitude = float(data["iss_position"]["longitude"])
-lat_long = [latitude, longitude]
-# print(latitude)
-# print(longitude)
+
+def iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    latitude = float(data["iss_position"]["latitude"])
+    longitude = float(data["iss_position"]["longitude"])
+    lat_long = [latitude, longitude]
+    if LAT-5 <= latitude <= LAT+5 and LONG-5 <= longitude <= LONG+5:
+        return True, lat_long
+    else:
+        return False, lat_long
+
 # # Calculate distance to ISS from current lat/long
 # gcd = EARTH_RADIUS * math.acos(
 #     (math.cos(LAT) * math.cos(latitude) * math.cos(LONG - longitude)) +
 #     (math.sin(LAT) * math.sin(latitude)))
 # print(gcd)
-if sunset_hour < hour_now.hour < sunrise_hour:
-    print("Night time.")
-    # if gcd <= visible_sky:
-    if LAT - 5 < latitude < LAT + 5:
-        print("ISS is visible")
-else:
-    print("Day time")
+
+
+while True:
+    overhead, lat_long = iss_overhead()
+    if is_night() and overhead:
+        time.sleep(60)
+        print(f"ISS should be visible in the sky at latitude: {lat_long[0]} and longitude: {lat_long[1]}")
+    else:
+        print(f"ISS is NOT visible at latitude: {lat_long[0]} and longitude: {lat_long[1]}")
+
 
