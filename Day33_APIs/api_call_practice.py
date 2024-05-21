a062cc57a2cd6d71857be52eb5f28a4aa8c9a5bb
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 13:56:34 -0700

    Adding GCD calculation but it isn't matching online calculators and need to debug.

--- Day33_APIs/api_call_practice.py
+++ Day33_APIs/api_call_practice.py
@@ -3,29 +3,53 @@ Start: May 21st, 2024
 Last touched: May 21st, 2024
 Author: Madeleine L.
 """
-# Resources: HTTP Status Code Glossary https://www.webfx.com/web-development/glossary/http-status-codes/
+# Resources:
+# HTTP Status Code Glossary https://www.webfx.com/web-development/glossary/http-status-codes/
+# Sunrise/Sunset time API: https://sunrise-sunset.org/api
 
 # Application Program Interfaces (APIs): is a set of commands, protocols, and objects that programmers can use to create
 # software or interact with an external system
 
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# print(response)  # Default is to get the response code
-# Response Codes Review -
-# 1XX: Hold on, something is happening
-# 2XX: Here you go, everything worked as expected
-# 3XX: You don't have permission to get this
-# 4XX: You screwed up. What you are looking for does not exist
-# 5XX: I screwed up (server is down or some other access issue on the websites side of things.)
-# print(response.status_code)
+LAT = 47.037872
+LONG = -122.900696
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # print(response)  # Default is to get the response code
+# # Response Codes Review -
+# # 1XX: Hold on, something is happening
+# # 2XX: Here you go, everything worked as expected
+# # 3XX: You don't have permission to get this
+# # 4XX: You screwed up. What you are looking for does not exist
+# # 5XX: I screwed up (server is down or some other access issue on the websites side of things.)
+# # print(response.status_code)
+# response.raise_for_status()
+# data = response.json()
+# latitude = data["iss_position"]["latitude"]
+# longitude = data["iss_position"]["longitude"]
+# lat_long = [latitude, longitude]
+# # print(lat_long)
+#
+# Required API input parameters
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0,
+ }
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-latitude = data["iss_position"]["latitude"]
-longitude = data["iss_position"]["longitude"]
-lat_long = [latitude, longitude]
-print(lat_long)
+sunrise_hour = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset_hour = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+hour_now = datetime.utcnow()
 
+if sunset_hour < hour_now.hour < sunrise_hour:
+    print("Night time.")
+else:
+    print("Day time")
 
 
 
--- Day33_APIs/main.py
+++ Day33_APIs/main.py
@@ -3,3 +3,55 @@ Start: May 21st, 2024
 Last touched: May 21st, 2024
 Author: Madeleine L.
 """
+import requests
+from datetime import datetime
+import math
+
+LAT = 47.037872
+LONG = -122.900696
+# HEIGHT_ABOVE_SEA = 61  # feet: 201 meters: 61
+EARTH_RADIUS = 6371000  # in meters
+HEIGHT = 1.75  # in meters
+PERCENT_SKY = 0.60
+
+# If the ISS is close to my current position and it is dark out
+# Run code every 60 seconds then send an email
+
+# API input parameters
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0,
+ }
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise_hour = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset_hour = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+hour_now = datetime.utcnow()
+
+# Estimate visible sky
+horizon_distance = (2*EARTH_RADIUS*HEIGHT) ** (1 / 2)
+visible_sky = horizon_distance*PERCENT_SKY
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+latitude = float(data["iss_position"]["latitude"])
+longitude = float(data["iss_position"]["longitude"])
+lat_long = [latitude, longitude]
+print(latitude)
+print(longitude)
+# Calculate distance to ISS from current lat/long
+gcd = EARTH_RADIUS * math.acos(
+    (math.cos(LAT) * math.cos(latitude) * math.cos(LONG - longitude)) +
+    (math.sin(LAT) * math.sin(latitude)))
+print(gcd)
+if sunset_hour < hour_now.hour < sunrise_hour:
+    print("Night time.")
+    if gcd <= visible_sky:
+        print("ISS is visible")
+else:
+    print("Day time")
+
