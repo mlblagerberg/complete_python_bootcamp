87c3befdae22e5ce849cdb495d7b1e80a996584a
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 14:49:52 -0700

    Removing usage of deprecated function utcnow.

--- Day33_APIs/main.py
+++ Day33_APIs/main.py
@@ -4,15 +4,15 @@ Last touched: May 21st, 2024
 Author: Madeleine L.
 """
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 import math
 
 LAT = 47.037872
 LONG = -122.900696
 # HEIGHT_ABOVE_SEA = 61  # feet: 201 meters: 61
-EARTH_RADIUS = 6371000  # in meters
-HEIGHT = 1.75  # in meters
-PERCENT_SKY = 0.60
+# EARTH_RADIUS = 6371000  # in meters
+# HEIGHT = 1.75  # in meters
+# PERCENT_SKY = 0.60
 
 # If the ISS is close to my current position and it is dark out
 # Run code every 60 seconds then send an email
@@ -29,11 +29,11 @@ response.raise_for_status()
 data = response.json()
 sunrise_hour = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset_hour = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-hour_now = datetime.utcnow()
+hour_now = datetime.now(timezone.utc)
 
-# Estimate visible sky
-horizon_distance = (2*EARTH_RADIUS*HEIGHT) ** (1 / 2)
-visible_sky = horizon_distance*PERCENT_SKY
+# # Estimate visible sky
+# horizon_distance = (2*EARTH_RADIUS*HEIGHT) ** (1 / 2)
+# visible_sky = horizon_distance*PERCENT_SKY
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -41,16 +41,17 @@ data = response.json()
 latitude = float(data["iss_position"]["latitude"])
 longitude = float(data["iss_position"]["longitude"])
 lat_long = [latitude, longitude]
-print(latitude)
-print(longitude)
-# Calculate distance to ISS from current lat/long
-gcd = EARTH_RADIUS * math.acos(
-    (math.cos(LAT) * math.cos(latitude) * math.cos(LONG - longitude)) +
-    (math.sin(LAT) * math.sin(latitude)))
-print(gcd)
+# print(latitude)
+# print(longitude)
+# # Calculate distance to ISS from current lat/long
+# gcd = EARTH_RADIUS * math.acos(
+#     (math.cos(LAT) * math.cos(latitude) * math.cos(LONG - longitude)) +
+#     (math.sin(LAT) * math.sin(latitude)))
+# print(gcd)
 if sunset_hour < hour_now.hour < sunrise_hour:
     print("Night time.")
-    if gcd <= visible_sky:
+    # if gcd <= visible_sky:
+    if LAT - 5 < latitude < LAT + 5:
         print("ISS is visible")
 else:
     print("Day time")
