ae0159bb3534ae7153eadf4ae12d1dfa39a9a199
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 11:15:22 -0700

    Automatically sending emails, to get this to work you have to go create app password in email provider and use that app pwd instead.

--- Day32_Automate_Emails/main.py
+++ Day32_Automate_Emails/main.py
@@ -4,6 +4,15 @@ Last touched: May 21st, 2024
 Author: Madeleine L.
 """
 # Simple Mail Transfer Protocol: Contains all the rules of how email is sent around the internet
-
-import smtplib
 # Skipping lesson for now as I don't want to create two new email addresses
+import smtplib
+
+EMAIL = "dummy1@provider.com"
+PWD = "alesio39jdl"
+
+with smtplib.SMTP("smtp.provider.com") as connection:
+    connection.starttls()  # Encrypts email so it cannot be read if intercepted
+    connection.login(user=EMAIL, password=PWD)
+    connection.sendmail(from_addr=EMAIL, to_addrs="dummy2@provider.com", msg="Subject: Hello Dummy 2"
+                                                                             "\n\n This is the body of my email")
+
