835a857c1ea51a6a3d0e40dd4c95f95ff40cc999
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 11:00:38 -0700

    Adding notes, but will not complete this exercise.

---
+++ Day32_Automate_Emails/main.py
@@ -0,0 +1,9 @@
+"""Project: Automate Emails
+Start: May 21st, 2024
+Last touched: May 21st, 2024
+Author: Madeleine L.
+"""
+# Simple Mail Transfer Protocol: Contains all the rules of how email is sent around the internet
+
+import smtplib
+# Skipping lesson for now as I don't want to create two new email addresses
