79da12f7107b6f18c48dfa0649cc0d297a716d96
mlblagerberg <mlblagerberg@gmail.com>
2024-05-21 11:32:37 -0700

    Datetime tutorial and adding quotes.

--- Day32_Automate_Emails/main.py
+++ Day32_Automate_Emails/main.py
@@ -6,13 +6,22 @@ Author: Madeleine L.
 # Simple Mail Transfer Protocol: Contains all the rules of how email is sent around the internet
 # Skipping lesson for now as I don't want to create two new email addresses
 import smtplib
+import datetime as dt
 
-EMAIL = "dummy1@provider.com"
-PWD = "alesio39jdl"
+# EMAIL = "dummy1@provider.com"
+# PWD = "alesio39jdl"
+#
+# with smtplib.SMTP("smtp.provider.com") as connection:
+#     connection.starttls()  # Encrypts email so it cannot be read if intercepted
+#     connection.login(user=EMAIL, password=PWD)
+#     connection.sendmail(from_addr=EMAIL, to_addrs="dummy2@provider.com", msg="Subject: Hello Dummy 2"
+#                                                                              "\n\n This is the body of my email")
 
-with smtplib.SMTP("smtp.provider.com") as connection:
-    connection.starttls()  # Encrypts email so it cannot be read if intercepted
-    connection.login(user=EMAIL, password=PWD)
-    connection.sendmail(from_addr=EMAIL, to_addrs="dummy2@provider.com", msg="Subject: Hello Dummy 2"
-                                                                             "\n\n This is the body of my email")
-
+current = dt.datetime.now()
+year = current.year
+month = current.month
+day = current.day
+# print(f"Year: {year}\nMonth: {month}\nDay: {day}")
+# Create a DOB
+date_of_birth = dt.datetime(year=2000, month=8, day=7)
+print(date_of_birth)
