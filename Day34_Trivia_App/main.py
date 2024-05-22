1964202e01b9c901ebc6787250c3d88a59523886
mlblagerberg <mlblagerberg@gmail.com>
2024-05-22 14:04:55 -0700

    replaceing random ten questions with API to call random 10 true/false questions.

--- Day34_Trivia_App/main.py
+++ Day34_Trivia_App/main.py
@@ -1,7 +1,26 @@
 """Project: Tkinter Trivia App
 Start: May 21st, 2024
-Last touched: May 21st, 2024
+Last touched: May 22nd, 2024
 Author: Madeleine L.
 """
 
 # Resources: https://opentdb.com/
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
