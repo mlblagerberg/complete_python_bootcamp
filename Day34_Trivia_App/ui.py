f89b4e74bdb8a7df98836e816b3dc48a099f9b93
mlblagerberg <mlblagerberg@gmail.com>
2024-05-23 11:44:13 -0700

    Changing label to canvas for question.

--- Day34_Trivia_App/ui.py
+++ Day34_Trivia_App/ui.py
@@ -16,13 +16,15 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.minsize(height=600, width=350)
         self.window.config(pady=20, padx=20, bg=THEME_COLOR)
-        # self.canvas = Canvas(width=350, height=600, bg=THEME_COLOR)
+
         self.score = 0
         self.score_label = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white", font=("Ariel", 15))
         self.score_label.grid(column=1, row=0)
 
-        self.question = Label(height=15, width=30, bg="white", font=("Ariel", 20, "italic"))
-        self.question.grid(column=0, row=1, columnspan=2, padx=30, pady=30)
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.question_text = self.canvas.create_text((150, 125), text="Question 1", fill=THEME_COLOR,
+                                                     font=("Ariel", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         false_png = PhotoImage(file=f"{IMAGE_PATH}/false.png")
         true_png = PhotoImage(file=f"{IMAGE_PATH}/true.png")
@@ -30,7 +32,6 @@ class QuizInterface:
         self.false_button.grid(column=1, row=2)
         self.true_button = Button(image=true_png)
         self.true_button.grid(column=0, row=2)
-
 
         self.window.mainloop()
 
