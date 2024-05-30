d5dec37f312a1a2b7c49318cfafb3ac6c46fd727
mlblagerberg <mlblagerberg@gmail.com>
2024-05-30 14:00:00 -0700

    Printing ID and forecast for all forecasts for next 5 days.

--- Day35_API_Auth/main.py
+++ Day35_API_Auth/main.py
@@ -14,7 +14,7 @@ sys.path.append("/Users/madeleinebeattylagerberg/GitHub/complete_python_bootcamp
 from constants_keys import WEATHER_API, LAT, LONG
 
 # OWM_API_CURRENT_WEATHER = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={WEATHER_API}"
-OWM_API_CALL = "https://api.openweathermap.org/data/2.5/forecast?"
+OWM_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast?"
 
 parameters = {
     "lat": LAT,
@@ -23,11 +23,14 @@ parameters = {
 }
 
 
-response = requests.get(url=OWM_API_CALL, params=parameters)
+response = requests.get(url=OWM_API_ENDPOINT, params=parameters)
 response.raise_for_status()
 print(response)  # Default to print response code
 data = response.json()
-print(data)
+# print(data)
 print(data.keys())
 
-print(data["list"][0]["weather"])  # Weather ID and description
+for forecast in range(len(data["list"])):
+    print(data["list"][forecast]["weather"])
+
+# print(data["list"][0]["weather"])  # Weather ID and description
