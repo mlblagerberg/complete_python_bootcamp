ecea567be69f1b14bb281b25aae8dbe134e6b944
mlblagerberg <mlblagerberg@gmail.com>
2024-05-30 13:42:34 -0700

    Initial API key pull.

--- Day35_API_Auth/main.py
+++ Day35_API_Auth/main.py
@@ -5,12 +5,19 @@ Author: Madeleine L.
 """
 # Resources:
 # https://home.openweathermap.org/users/sign_up
+# https://openweathermap.org/forecast5
+# https://jsonviewer.stack.hu/
+
 import requests
 import sys
 sys.path.append("/Users/madeleinebeattylagerberg/GitHub/complete_python_bootcamp")
-from api_keys import WEATHER_API, LAT, LONG
+from constants_keys import WEATHER_API, LAT, LONG
 
 URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={WEATHER_API}"
 
 response = requests.get(url=URL)
-print(response.text)
+response.raise_for_status()
+print(response)  # Default to print response code
+data = response.json()
+print(data)
+print(data.keys())
