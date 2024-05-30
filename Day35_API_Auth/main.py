2b08b4aa174dace27fa6cbdcf3013bab367b42dd
mlblagerberg <mlblagerberg@gmail.com>
2024-05-30 13:55:01 -0700

    Getting weather ID and decription from output.

--- Day35_API_Auth/main.py
+++ Day35_API_Auth/main.py
@@ -13,11 +13,21 @@ import sys
 sys.path.append("/Users/madeleinebeattylagerberg/GitHub/complete_python_bootcamp")
 from constants_keys import WEATHER_API, LAT, LONG
 
-URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={WEATHER_API}"
+# OWM_API_CURRENT_WEATHER = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={WEATHER_API}"
+OWM_API_CALL = "https://api.openweathermap.org/data/2.5/forecast?"
 
-response = requests.get(url=URL)
+parameters = {
+    "lat": LAT,
+    "lon": LONG,
+    "appid": WEATHER_API,
+}
+
+
+response = requests.get(url=OWM_API_CALL, params=parameters)
 response.raise_for_status()
 print(response)  # Default to print response code
 data = response.json()
 print(data)
 print(data.keys())
+
+print(data["list"][0]["weather"])  # Weather ID and description
