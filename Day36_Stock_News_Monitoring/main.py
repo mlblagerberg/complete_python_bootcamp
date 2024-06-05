03ec95d13e5acf42df374fe8f5c9caa22a699d2e
mlblagerberg <mlblagerberg@gmail.com>
2024-06-05 16:21:29 -0700

    TODOs and api resources.

--- Day36_Stock_News_Monitoring/main.py
+++ Day36_Stock_News_Monitoring/main.py
@@ -4,3 +4,12 @@ Last touched: June 5th, 2024
 Author: Madeleine L.
 """
 
+# TODO 1: Get stock price delta between opening and previous day close
+# TODO 2: If it was a large fluctuation (>10%) then find related news articles for company
+# TODO 3: Send notification with fluctuation and any relevant articles
+
+# # Resources
+# Stock Price API: https://www.alphavantage.co/
+# News API: https://newsapi.org/
+# Twilio API: https://www.twilio.com/en-us
+
