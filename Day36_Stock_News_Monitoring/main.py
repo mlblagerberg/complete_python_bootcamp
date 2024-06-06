6b3e2a9406a1e4d09e08c425ac96d8b9565f1cee
mlblagerberg <mlblagerberg@gmail.com>
2024-06-06 15:31:58 -0700

    Pulling in Alpha Vantage API and getting today and yesterdays stock value/volume.

--- Day36_Stock_News_Monitoring/main.py
+++ Day36_Stock_News_Monitoring/main.py
@@ -4,7 +4,32 @@ Last touched: June 5th, 2024
 Author: Madeleine L.
 """
 
-# TODO 1: Get stock price delta between opening and previous day close
+import requests
+from datetime import datetime, timedelta
+import os
+
+api_key = os.getenv("ALPHA_VANTAGE_API")
+# TODO 1: Get stock price delta between current day opening and previous day close
+parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": "ENPH",
+    "outputsize": "compact",
+    "apikey": api_key,
+}
+
+url = "https://www.alphavantage.co/query?"
+response = requests.get(url, params=parameters)
+data = response.json()
+# print(data)
+
+today = datetime.today().strftime("%Y-%m-%d")
+yesterday = (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")
+
+today_stock = data["Time Series (Daily)"][today]
+yesterday_stock = data["Time Series (Daily)"][yesterday]
+print(f"Today's stock details: {today_stock}")
+print(f"Yesterday's stock details: {yesterday_stock}")
+
 # TODO 2: If it was a large fluctuation (>10%) then find related news articles for company
 # TODO 3: Send notification with fluctuation and any relevant articles
 
