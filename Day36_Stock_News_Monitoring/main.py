d6179ef714d1c1bef256cb65a2d233c208094f18
mlblagerberg <mlblagerberg@gmail.com>
2024-06-18 19:40:14 -0700

    Calculating 10% shift and adding conditional to pull news resources.

--- Day36_Stock_News_Monitoring/main.py
+++ Day36_Stock_News_Monitoring/main.py
@@ -30,30 +30,36 @@ stock_parameters = {
 stock_url = "https://www.alphavantage.co/query?"
 response = requests.get(stock_url, params=stock_parameters)
 stock_data = response.json()
-print(stock_data)
+# print(stock_data)
 
 today_stock = stock_data["Time Series (Daily)"][today]
 yesterday_stock = stock_data["Time Series (Daily)"][yesterday]
 #
 print(f"Today's stock details: {today_stock}")
+print(f"Yesterday's stock details: {yesterday_stock}")
 # print(f"Yesterday's stock details: {yesterday_stock}")
+open_price = float(today_stock["1. open"])
+close_price = float(yesterday_stock["4. close"])
+price_delta = close_price - open_price
+print(round((price_delta/close_price)*100, 2))
 
-# TODO 2: If it was a large fluctuation (>10%) then find related news articles for company from the past week
-news_api_key = os.getenv("NEWS_API")
+if round((price_delta/close_price)*100, 2) >= 10:
+    # TODO 2: If it was a large fluctuation (>10%) then find related news articles for company from the past week
+    news_api_key = os.getenv("NEWS_API")
 
-news_url = "https://newsapi.org/v2/everything?"
+    news_url = "https://newsapi.org/v2/everything?"
 
-news_parameters = {
-    "q": "ENPH",
-    "from": yesterday,
-    "sortBy": "popularity",
-    "apiKey": news_api_key,
-}
+    news_parameters = {
+        "q": "ENPH",
+        "from": yesterday,
+        "sortBy": "popularity",
+        "apiKey": news_api_key,
+    }
 
-news_response = requests.get(news_url, params=news_parameters)
-news_data = news_response.json()
-# print(dumps(news_data))
-print(news_data)
+    news_response = requests.get(news_url, params=news_parameters)
+    news_data = news_response.json()
+    # print(dumps(news_data))
+    # print(news_data)
 
 # TODO 3: Send notification with fluctuation and any relevant articles
 
