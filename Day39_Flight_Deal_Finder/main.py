16abc7b5f6aa53a0ae1943d2d319abb3d332e8dc
mlblagerberg <mlblagerberg@gmail.com>
2025-04-12 09:27:09 -0700

    Adding files that have been untracked.

--- Day39_Flight_Deal_Finder/main.py
+++ Day39_Flight_Deal_Finder/main.py
@@ -1,6 +1,12 @@
 """Project: Flight Deal Finder
 Start: August 7th, 2024
-Last touched: August 7th, 2024
+Last touched: November 27th, 2024
 Author: Madeleine L.
 """
 
+import requests
+from datetime import datetime, timedelta
+import os
+
+# # Resources
+# Twilio API: https://www.twilio.com/en-us
---
+++ Day41_Web_Foundation_HTML_Intro/main.py
@@ -0,0 +1,12 @@
+"""Project: HTML Intro
+Start: December 10th, 2024
+Last touched: December 10th, 2024
+Author: Madeleine L.
+"""
+
+# HTML File: Responsible for structure of internet page. E.g. Walls.
+# CSS File: Responsible for styling your website. E.g. color.
+# JS File: Responsible for site functionality. E.g. Wires.
+
+
+
